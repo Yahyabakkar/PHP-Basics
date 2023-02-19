bd4fd10a439c451319910128e18c918ccc31324d
yahyabakkar <yahyabakkar022@gmail.com>
2023-02-19 18:06:28 +0200

    step3.php

---
+++ basics1/step3.php
@@ -0,0 +1,9 @@
+<?php
+$name = "yahya";
+$email = "yahyabakkar022@gmail.com";
+$height = "20";
+$width = "50";
+$area = "$height * $width";
+echo "My name is $name and my email is $email\n";
+echo "The area value is $area \n";
+?>
