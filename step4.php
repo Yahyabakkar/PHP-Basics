78c4f7bc4e228f8282a203425bcc8f951a582ca5
yahyabakkar <yahyabakkar022@gmail.com>
2023-02-19 18:16:31 +0200

    step4.php

---
+++ step4.php
@@ -0,0 +1,8 @@
+<?php
+$name = 'yahya';
+$position = "full-stack web developer";
+$github_url = "https://github.com/Yahyabakkar";
+echo "Hello, I'm " .$name.", I'm " .$position. " please check my github link " .$github_url. "\n";
+echo "Hello, I'm $name, I'm $position please check my github link $github_url \n";
+echo 'Hello, I\'m ' .$name.', I\'m ' .$position. ' please check my github link ' .$github_url. "\n";
+?>
