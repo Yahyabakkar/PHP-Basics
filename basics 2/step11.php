bdf65a4ba315fd2476427de644c94ed328484042
yahyabakkar <yahyabakkar022@gmail.com>
2023-02-20 02:23:49 +0200

    step11.php

---
+++ basics 2/step11.php
@@ -0,0 +1,14 @@
+<?php
+function primeCheck($number)
+{
+    if ($number == 1)
+        return "{$number} is not a prime number";
+    for ($i = 2; $i <= $number / 2; $i++) {
+        if ($number % $i == 0)
+            return "{$number} is not a prime number";
+    }
+    return "{$number} is a prime number";
+}
+echo primeCheck(1), "\n";
+echo primeCheck(7), "\n";
+?>
